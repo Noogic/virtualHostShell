518448f056b4a91749390e6c05a14be69592c92a
Joan Morell Andres <noogic@gmail.com>
2015-08-21 00:14:49 +0200

    Added messages to shell

--- shell.php
+++ shell.php
@@ -2,5 +2,17 @@
 namespace virtualhost;
 
 class Shell{
-	
+	private $input;
+    private $user_data = [];
+    private $msg = [];
+
+    function __construct(Input $input){
+        $this->input = $input;
+    	$this->loadMessages();
+    }
+
+
+    private function loadMessages(){
+        $this->msg['welcome'] = "Welcome to de virtual host creator. Let's get started\n\n";
+    }
 }
