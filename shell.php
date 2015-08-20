e0d295c9c55744bb5b1d6096e9facf93450cfe32
Joan Morell Andres <noogic@gmail.com>
2015-08-21 00:21:00 +0200

    Added start method to Shell and domain_type message and input

--- shell.php
+++ shell.php
@@ -12,7 +12,15 @@ class Shell{
 	}
 
 
+	public function start(){
+		echo $this->msg['welcome'];
+		echo $this->msg['domain_type'];
+
+		$this->user_data['domain_type'] = $this->input->domain_type(fgets(STDIN));
+	}
+
 	private function loadMessages(){
 		$this->msg['welcome'] = "Welcome to de virtual host creator. Let's get started\n\n";
+		$this->msg['domain_type'] = "Are you going to create a domain (d)(by default) or subdomain (s)?: ";
 	}
 }
