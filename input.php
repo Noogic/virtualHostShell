318d699b7d1ff8ad2ef569df8ba060b6884694a7
Joan Morell Andres <noogic@gmail.com>
2015-08-21 12:47:16 +0200

    Removed new lines from user input data

--- input.php
+++ input.php
@@ -3,7 +3,7 @@ namespace virtualhost;
 
 class Input{
 	public function domain_type($data){
-		$data = trim(strtolower($data));
+		$data = clear($data);
 
 		return ($data == 's' or $data == 'subdomain') ? 'subdomain' : 'domain';
 	}
@@ -12,11 +12,11 @@ class Input{
 		if(!preg_match('/[a-z0-9]+\.[a-z]+/', $data))
 			throw new \UnexpectedValueException("Invalid domain $data");
 
-		return trim(strtolower($data));
+		return $this->clear($data, true);
 	}
 
 	public function create_user($data){
-		$data = trim(strtolower($data));
+		$data = $this->clear($data, true);
 
 		return ($data == 'y' or $data == 'yes');
 	}
@@ -29,6 +29,14 @@ class Input{
 		if($password != $re_password)
 			throw new \UnexpectedValueException("Password doesn't match");
 
-		return $password;
+		return $this->clear($password);
+	}
+
+	private function clear($input, $lowercase = false){
+		$output = trim($input);
+		$output = str_replace("\n", '', $output);
+		$output = str_replace("\r", '', $output);
+
+		return $lowercase ? strtolower($output) : $output;
 	}
 }
